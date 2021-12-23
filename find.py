ec946928f907df8211fbe6f383c95aef3ed63647
lahbabic <lahbabic@gmail.com>
2021-12-23 19:22:11 +0100

    final version with example

--- find.py
+++ find.py
@@ -1,11 +1,16 @@
-def find(espace0, item, args):
-    for x in espace0:
-        if x == item:
-            args.append(espace0.index(x))
-            yield args
+espace = [[[[[0], 1], 5, [[2]]], 3], 4]
+
+def find(espace, item, coordonnee):
+    for dimension in espace:
+        if dimension == item:
+            coordonnee.append(espace.index(dimension))
+            yield coordonnee
         else:
-            if isinstance(x, list):
-                for z in find(x, item, args):
-                    args = list(z)
-                    args.append(espace0.index(x))
-                    yield args[::-1]
+            if isinstance(dimension, list):
+                for x in find(dimension, item, coordonnee):
+                    coordonnee = list(x)
+                    coordonnee.append(espace.index(dimension))
+                    yield coordonnee
+
+for x in find(espace, 2, []):
+    print(x)
